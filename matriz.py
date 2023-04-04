a27e55a73abcb8c192d6e2be6dd7f15c9b8fc6d7
GuiFreittas <fbguiedits26@gmail.com>
2023-04-03 21:50:26 -0300

    Gui

--- matriz.py
+++ matriz.py
@@ -1,1 +1,9 @@
-teste
+matriz = [[1, 2, 3], [4, 5, 6]]
+
+soma = 0
+
+for linha in matriz:
+    for elemento in linha:
+        soma += elemento
+
+print(f"A soma de todos os elementos da matriz é: {soma}")
